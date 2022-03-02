ef955e63f1be5f06fc0857920a2e84f1bc93dc12
ARCHIT-PC\Archit <archit.poddar@gmail.com>
2022-03-03 01:13:20 +0530

    * Setup project

--- MyCoffeeApp/MyCoffeeApp/App.xaml.cs
+++ MyCoffeeApp/MyCoffeeApp/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using MyCoffeeApp.Services;
-using MyCoffeeApp.Views;
-using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace MyCoffeeApp
 {
@@ -13,7 +9,7 @@ namespace MyCoffeeApp
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
+            //DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
 
