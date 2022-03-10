810bf91ed05753fb7440c7f95f1e9031d5bc806e
ARCHIT-PC\Archit <archit.poddar@gmail.com>
2022-03-11 00:34:33 +0530

    * Added ListView for Coffee

--- MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using MvvmHelpers;
 using MvvmHelpers.Commands;
+using MyCoffeeApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -12,22 +16,60 @@ namespace MyCoffeeApp.ViewModels
 {
     public class CoffeeEquipmentViewModel : ViewModelBase /*BindableObject*/
     {
-        public ObservableRangeCollection<string> Coffee { get; }
+        public ObservableRangeCollection<Coffee> Coffee { get; set; }
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeeGroups { get; }
+
+        public AsyncCommand RefreshCommand { get; }
+        public AsyncCommand<Coffee> FavoriteCommand { get; }
+
         public CoffeeEquipmentViewModel()
         {
+            Title = "Coffee Equipment";
+;
+            Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+
+            var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png";
+
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Blue Bottle", new[] { Coffee[2] }));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Yes Plz", Coffee.Take(2)));
 
             CallServerCommand = new AsyncCommand(DoCallServerCommand);
             IncreaseCountCommand = new Command(DoIncreaseCountCommand);
+            RefreshCommand = new AsyncCommand(DoRefreshCommand);
+            FavoriteCommand = new AsyncCommand<Coffee>(DoFavoriteCommand);
+        }
+
+        private async Task DoFavoriteCommand(Coffee coffee)
+        {
+            
+        }
+
+        private async Task DoRefreshCommand()
+        {
+            IsBusy = true;
+
+            await Task.Delay(2000);
+
+            IsBusy = false;
         }
 
         private async Task DoCallServerCommand()
         {
-            var items = new List<string>();
-            items.Add("Coffee 1");
-            items.Add("Coffee 2");
-            items.Add("Coffee 3");
+            //var items = new List<string>();
+            //items.Add("Coffee 1");
+            //items.Add("Coffee 2");
+            //items.Add("Coffee 3");
 
-            Coffee.AddRange(items);
+            //Coffee.AddRange(items);
         }
 
         private void DoIncreaseCountCommand()
@@ -62,5 +104,16 @@ namespace MyCoffeeApp.ViewModels
             }
         }
 
+        //protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        //{
+        //    if (!Equals(field, newValue))
+        //    {
+        //        field = newValue;
+        //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
     }
 }
--- MyCoffeeApp/MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
+++ MyCoffeeApp/MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MyCoffeeApp.ViewModels;
+﻿using MyCoffeeApp.Models;
+using MyCoffeeApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,42 @@ namespace MyCoffeeApp.Views
 
             //BindingContext = new CoffeeEquipmentViewModel();
             //LabelCount.Text = "Text from code behind directly.";
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var listView = sender as ListView;
+
+            var coffee = listView.SelectedItem as Coffee;
+
+            if (coffee == null)
+                return;
+
+            var name = coffee.Name;
+
+            await DisplayAlert("Selected", name, "OK");
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var listView = sender as ListView;
+
+            listView.SelectedItem = null;
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+
+            var coffee = menuItem.BindingContext as Coffee;
+
+            if (coffee == null)
+                return;
+
+            var name = coffee.Name;
+
+            await DisplayAlert("Favourited", name, "OK");
+
         }
     }
 }
